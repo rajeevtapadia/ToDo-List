4ca228721e1c2909190147078996b07af86fbe5d
Rajeev Tapadia <rtapadia730@gmail.com>
2023-08-05 01:13:53 +0530

    task shuffling

--- script.js
+++ script.js
@@ -20,9 +20,10 @@ input.addEventListener('keydown', event => {
 });
 
 list.addEventListener('click', event => {
-    console.log(event);
+    console.log(event.target);
     if(event.target.tagName === 'LI'){
         event.target.classList.toggle('checked');
+        shuffle(event);
         save();
     }
     else if(event.target.tagName == 'SPAN'){
@@ -40,6 +41,22 @@ const display = () => {
 // function to save task in localstorage
 const save = () => {
     localStorage.setItem('taskList', list.innerHTML);
+}
+
+// function to append completed tasks at bottom
+const shuffle = (event) => {
+    console.log(event.target.classList);
+    let checkedTask = event.target.outerHTML;
+    if('checked' === event.target.classList[0]){
+        // shift down
+        event.target.remove();
+        list.innerHTML += checkedTask
+    }
+    else{
+        // shift up
+        event.target.remove();
+        list.innerHTML = checkedTask + list.innerHTML;
+    }
 }
 
 display();
